3994d20a0c449164bca47687269fbc6d0ed64eeb
KYIV\Pavlo_Dmytrenko <Pavlo_Dmytrenko@epam.com>
2021-08-02 20:31:33 +0300

    commit changes

--- EF+linq/Program.cs
+++ EF+linq/Program.cs
@@ -13,26 +13,29 @@ namespace EF_linq
 
             #region addNewDataToTables
             /*
-            // создаем два объекта User
-            Users user1 = new Users { Name = "Tom" };
-            Users user2 = new Users { Name = "Sam" };
+            using (UserContext db = new UserContext())
+            {
+                // создаем два объекта User
+                Users user1 = new Users {Name = "Tom"};
+                Users user2 = new Users {Name = "Sam"};
 
-            // добавляем их в бд
-            db.Users.Add(user1);
-            db.Users.Add(user2);
-            db.SaveChanges();
+                // добавляем их в бд
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.SaveChanges();
 
-            Photos pl1 = new Photos { Context = "meeting", CreatedDate = DateTime.Now,  Users = user1 };
-            Photos pl2 = new Photos { Context = "daily", CreatedDate = DateTime.Now,  Users = user1 };
-            Photos pl3 = new Photos { Context = "dogs", CreatedDate = DateTime.Now,  Users = user2};
-            db.Photos.AddRange(new List<Photos> { pl1, pl2, pl3 });
-            db.SaveChanges();
+                Photos pl1 = new Photos {Context = "meeting", CreatedDate = DateTime.Now, Users = user1};
+                Photos pl2 = new Photos {Context = "daily", CreatedDate = DateTime.Now, Users = user1};
+                Photos pl3 = new Photos {Context = "dogs", CreatedDate = DateTime.Now, Users = user2};
+                db.Photos.AddRange(new List<Photos> {pl1, pl2, pl3});
+                db.SaveChanges();
 
-            Likes lk1 = new Likes { CreatedDate = DateTime.Now, Photos = pl1 };
-            Likes lk2 = new Likes { CreatedDate = DateTime.Now, Photos = pl1 };
-            Likes lk3 = new Likes { CreatedDate = DateTime.Now, Photos = pl2 };
-            db.Likes.AddRange(new List<Likes> { lk1, lk2, lk3 });
-            db.SaveChanges();
+                Likes lk1 = new Likes {CreatedDate = DateTime.Now, Photos = pl1};
+                Likes lk2 = new Likes {CreatedDate = DateTime.Now, Photos = pl1};
+                Likes lk3 = new Likes {CreatedDate = DateTime.Now, Photos = pl2};
+                db.Likes.AddRange(new List<Likes> {lk1, lk2, lk3});
+                db.SaveChanges();
+            }
             */
             #endregion
             DBOperation dBOperation = new DBOperation();
