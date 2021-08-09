9dd9bbd46f457277fa8c193a8e1a94bebb9916cb
KYIV\Pavlo_Dmytrenko <Pavlo_Dmytrenko@epam.com>
2021-08-09 11:20:38 +0300

    Add  new tests  and changed projects according to the Review file -  https://epam-my.sharepoint.com/personal/pavlo_dmytrenko_epam_com/Documents/Suggestions%20for%20EF_linq%20project.docx?web=1

---
+++ EF+LinqTest/DBOperationTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using EF_linq;
+
+namespace TestLinq
+{
+    public class DBOperationTest
+    {
+        DBOperation dBOperation = new DBOperation();
+
+        [Fact]
+        public void DBOperationTestCountUsers()
+        {
+            Assert.Equal(2, dBOperation.SelectItem(Convert.ToDateTime(DateTime.Today.AddMonths(-1))).Count);
+        }
+        [Fact]
+        public void DBOperationTestCountLikes()
+        {
+            Assert.Equal(0, dBOperation.SelectItem(Convert.ToDateTime(DateTime.Today.AddMonths(-1))).Where(x => x.UserId == 1).Select(x => x.Count).First());
+        }
+        [Fact]
+        public void DBOperationTestPeriod()
+        {
+            Assert.Equal(3, dBOperation.SelectItem(Convert.ToDateTime(DateTime.Today.AddMonths(-1))).Where(x => x.UserId == 1).Select(x => x.Count).First());
+            var resItemMoreMonth = dBOperation.SelectItem(Convert.ToDateTime(DateTime.Today.AddMonths(-2)));
+            Assert.Equal(3, resItemMoreMonth.Where(x => x.UserId == 1).Select(x => x.Count).First());
+        }
+    }
+}
--- EF+linq/Program.cs
+++ EF+linq/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,38 +11,12 @@ namespace EF_linq
     {
         static void Main(string[] args)
         {
-
-            #region addNewDataToTables
-            /*
-            using (UserContext db = new UserContext())
-            {
-                // создаем два объекта User
-                Users user1 = new Users {Name = "Tom"};
-                Users user2 = new Users {Name = "Sam"};
-
-                // добавляем их в бд
-                db.Users.Add(user1);
-                db.Users.Add(user2);
-                db.SaveChanges();
-
-                Photos pl1 = new Photos {Context = "meeting", CreatedDate = DateTime.Now, Users = user1};
-                Photos pl2 = new Photos {Context = "daily", CreatedDate = DateTime.Now, Users = user1};
-                Photos pl3 = new Photos {Context = "dogs", CreatedDate = DateTime.Now, Users = user2};
-                db.Photos.AddRange(new List<Photos> {pl1, pl2, pl3});
-                db.SaveChanges();
-
-                Likes lk1 = new Likes {CreatedDate = DateTime.Now, Photos = pl1};
-                Likes lk2 = new Likes {CreatedDate = DateTime.Now, Photos = pl1};
-                Likes lk3 = new Likes {CreatedDate = DateTime.Now, Photos = pl2};
-                db.Likes.AddRange(new List<Likes> {lk1, lk2, lk3});
-                db.SaveChanges();
-            }
-            */
-            #endregion
             DBOperation dBOperation = new DBOperation();
-            var resItem = dBOperation.SelectItem();
-            foreach (var res in  resItem)
+            var resItem = dBOperation.SelectItem(Convert.ToDateTime(DateTime.Today.AddMonths(-1)));
+            foreach (var res in resItem)
+            {
                 Console.WriteLine(res.UserId + "  " + res.Name + " " + res.Count);
+            }
             Console.Read();
         }
     }
